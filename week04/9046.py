ade151bc52e8acb573dea4d33a6bb50f5c17a110
hoon <devkwonsehoon@naver.com>
2021-09-08 22:35:55 +0900

    update: 9046.py

--- week04/9046.py
+++ week04/9046.py
@@ -1,9 +1,9 @@
 for _ in range(int(input())):
-    alphabet_index = [0] * 26
+    alphabet_index = [0] * 26  # 알파벳 인덱스
     password = input()
 
     for x in password:
-        if x.isalpha():
+        if x.isalpha():  # 공백 미포함하기 위해 isalpha() 사용
             alphabet_index[ord(x)-ord('a')] += 1
 
     mods = max(alphabet_index)
